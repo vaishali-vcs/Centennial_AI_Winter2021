5ac2a5c04f8f5e66519f91294dac9a61816d53fc
vaishali-vcs <vaishali.bhat@yahoo.com>
2021-02-22 19:22:52 -0500

    changed local db to cloud mongodb. ready to be hosted on heroku
    changed local db to cloud mongodb. ready to be hosted on heroku

--- mongodb_nodejs_auth_demo/server/config/db.js
+++ mongodb_nodejs_auth_demo/server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost:27017/book_store"
+    // "URI": "mongodb://localhost:27017/book_store"
+    "URI": "mongodb+srv://vaishali:QwwL92a2FkGuZkXy@cluster0.srcwm.mongodb.net/book_store?retryWrites=true&w=majority"
 }
