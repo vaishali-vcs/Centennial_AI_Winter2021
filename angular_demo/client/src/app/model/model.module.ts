228fc686a7bf7de45f79d1d01f4e2d335beee02c
vaishali-vcs <vaishali.bhat@yahoo.com>
2021-03-18 10:24:15 -0400

    angular with json server data
    angular with json server data

--- angular_demo/client/src/app/model/model.module.ts
+++ angular_demo/client/src/app/model/model.module.ts
@@ -2,8 +2,14 @@ import {NgModule} from '@angular/core';
 import {BookRepository} from './book.repository';
 import {StaticDataSource} from './static.datasource';
 import { Cart } from './cart.model';
+import { HttpClientModule } from '@angular/common/http';
+import { RestDataSource } from './rest.datasource';
 
-@NgModule({providers: [BookRepository, StaticDataSource, Cart]})
+@NgModule({
+  imports: [HttpClientModule],
+  providers: [BookRepository, StaticDataSource, Cart,
+  {provide: StaticDataSource, useClass: RestDataSource}]
+})
 export class ModelModule {
 
 }
--- angular_demo/client/src/app/partials/partials.module.ts
+++ angular_demo/client/src/app/partials/partials.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CartSummaryComponent } from '../book-store/cart-summary/cart-summary.component';
 
 
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, CartSummaryComponent],
   exports: [HeaderComponent, FooterComponent]
 })
 export class PartialsModule{
