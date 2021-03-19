aff695a642a9c5b3e515656bd56524d2525eaa41
vaishali-vcs <vaishali.bhat@yahoo.com>
2021-03-19 09:09:43 -0400

    angular with json server datasource
    angular with json server datasource

--- angular_nodejs_demo/client/src/app/partials/partials.module.ts
+++ angular_nodejs_demo/client/src/app/partials/partials.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { CartSummaryComponent } from '../book-store/cart-summary/cart-summary.component';
 
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [HeaderComponent, FooterComponent],
-  exports: [HeaderComponent, FooterComponent]
+  declarations: [HeaderComponent, FooterComponent, CartSummaryComponent],
+  exports: [HeaderComponent, FooterComponent, CartSummaryComponent]
 })
 export class PartialsModule{
 
